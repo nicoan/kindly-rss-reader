f0e6d69961749ff3e4fae861b141b238ce5fb161
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-03-18 19:32:00 -0300

    refactor: tidy up code

--- src/controllers/feed/delete_feed.rs
+++ src/controllers/feed/delete_feed.rs
@@ -1,8 +1,7 @@
-use crate::services::feed::FeedService;
 use crate::state::AppState;
+use crate::{controllers::ApiError, services::feed::FeedService};
 use axum::{
     extract::{Path, State},
-    http::StatusCode,
     response::Redirect,
 };
 use uuid::Uuid;
@@ -10,15 +9,8 @@ use uuid::Uuid;
 pub async fn delete_feed<S: AppState>(
     State(state): State<S>,
     Path(feed_id): Path<Uuid>,
-) -> Result<Redirect, (StatusCode, &'static str)> {
-    state
-        .feed_service()
-        .delete_feed(feed_id)
-        .await
-        .map_err(|e| {
-            tracing::error!("Error deleting feed: {e}");
-            (StatusCode::INTERNAL_SERVER_ERROR, "Failed to delete feed")
-        })?;
-    
+) -> Result<Redirect, ApiError> {
+    state.feed_service().delete_feed(feed_id).await?;
+
     Ok(Redirect::to("/"))
 }
