8b8df8c9a58192d1e2fcd3279d6e37aa3f882be4
Nicolás Antinori <nico.antinori.7@gmail.com>
2026-08-02 20:10:48 -0300

    fix: invert fav icons in dark theme (#33)
    Favicons icon were inverted when using dark theme. This was not desired
    behaviour since inverting the images is for articles only.
    
    This also allowed to remove some dark images using the css color
    inversion.

--- src/view_models/error.rs
+++ src/view_models/error.rs
@@ -3,7 +3,6 @@ use serde::Serialize;
 #[derive(Serialize)]
 pub struct Error {
     message: &'static str,
-    error_image_path_dark: &'static str,
     error_image_path: &'static str,
 }
 
@@ -11,7 +10,6 @@ impl Error {
     pub fn not_found() -> Self {
         Self {
             message: "Page not found.",
-            error_image_path_dark: "/static/images/not_found_dark.webp",
             error_image_path: "/static/images/not_found.webp",
         }
     }
@@ -19,7 +17,6 @@ impl Error {
     pub fn internal_error() -> Self {
         Self {
             message: "An unexpected error ocurred. Please check the logs.",
-            error_image_path_dark: "/static/images/internal_error_dark.webp",
             error_image_path: "/static/images/internal_error.webp",
         }
     }
@@ -27,7 +24,6 @@ impl Error {
     pub fn bad_request() -> Self {
         Self {
             message: "Bad request. Please check the url.",
-            error_image_path_dark: "/static/images/bad_request_dark.webp",
             error_image_path: "/static/images/bad_request.webp",
         }
     }
