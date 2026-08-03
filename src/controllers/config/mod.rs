ec8b6eb5324f7d3b73fd744dc76f0505cf7be3a4
Nicolás Antinori <nico.antinori.7@gmail.com>
2026-08-02 21:33:48 -0300

    add: configuration page

--- src/controllers/config/mod.rs
+++ src/controllers/config/mod.rs
@@ -1,9 +1,11 @@
 // SPDX-FileCopyrightText: 2025-2026 Nicolás Antinori <nico.antinori.7@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-only
+mod get_config;
 mod set_dark_theme;
 mod set_toolbar_position_left;
 mod set_zoom;
 
+pub use get_config::get_config;
 pub use set_dark_theme::set_dark_theme;
 pub use set_toolbar_position_left::set_toolbar_position_left;
 pub use set_zoom::set_zoom;
